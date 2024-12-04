8892c5852c6ebbaea6f5fc5922dac838bfff6d80
max529 <maxime.betrisey@hotmail.com>
2024-12-04 22:55:16 +0100

    Remove env config

--- DefaultProject/Configuration.cs
+++ DefaultProject/Configuration.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using AventusSharp.Tools;
+using AventusSharp.Tools.Attributes;
 
 namespace ${{projectName}};
 
@@ -34,53 +36,5 @@ public class DatabaseConfig
     public override string ToString()
     {
         return $"{Host}:{Port} {Database} {Username} {Password}";
-    }
-}
-
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-public class EnvName : Attribute
-{
-    internal string Name { get; private set; }
-    public EnvName(string name)
-    {
-        Name = name;
-    }
-}
-public class EnvConfig
-{
-    public static T? Load<T>()
-    {
-        Type t = typeof(T);
-        T? config = (T?)Activator.CreateInstance(t);
-
-        if (config == null) return default;
-
-        PropertyInfo[] properties = t.GetProperties();
-        foreach (PropertyInfo property in properties)
-        {
-            EnvName? envName = property.GetCustomAttribute<EnvName>();
-            if (envName != null)
-            {
-                string? envValue = Environment.GetEnvironmentVariable(envName.Name);
-                Type valueType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
-                object? convertedValue = envValue != null ? Convert.ChangeType(envValue, valueType) : null;
-                property.SetValue(config, convertedValue);
-            }
-        }
-
-        FieldInfo[] fields = t.GetFields();
-        foreach (FieldInfo field in fields)
-        {
-            EnvName? envName = field.GetCustomAttribute<EnvName>();
-            if (envName != null)
-            {
-                string? envValue = Environment.GetEnvironmentVariable(envName.Name);
-                Type valueType = Nullable.GetUnderlyingType(field.FieldType) ?? field.FieldType;
-                object? convertedValue = envValue != null ? Convert.ChangeType(envValue, valueType) : null;
-                field.SetValue(config, convertedValue);
-            }
-        }
-
-        return config;
     }
 }

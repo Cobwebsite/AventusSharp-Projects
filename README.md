4134ed1f5089be5181f54e73295c514e3d3c03ad
Maxime <maxime.betrisey@hotmail.com>
2025-10-20 08:12:54 +0200

    Update for the store

---
+++ README.md
@@ -0,0 +1,23 @@
+<p style="display:flex;justify-content:center">
+    <img src="https://aventusjs.com/img/aventussharp.svg" alt="AventusSharp" height="250" />
+</p>
+
+# AventusSharp
+
+AventusSharp is a versatile C# package designed to streamline the development of modern applications. By providing robust tools for data modeling, business logic management, API routing, and frontend integration, AventusSharp empowers developers to focus on building features rather than handling repetitive tasks.
+
+Whether you're working on a small project or a large-scale application, AventusSharp simplifies complex processes, ensuring consistency, efficiency, and flexibility across your application stack.
+
+
+## Documentation
+
+- **Aventus Docs:** [https://aventusjs.com](https://aventusjs.com)
+- **AventusSharp Docs:** [https://aventussharp.com](https://aventussharp.com)
+
+## ⚙️ Quick Installation
+
+To get started with AventusSharp, simply download the official project template directly inside VS Code:
+
+[https://store.aventusjs.com/templates/AventusSharp.Project](https://store.aventusjs.com/templates/AventusSharp.Project)
+
+Once installed, open VS Code and create a new project with the Aventus extension by right clicking on the explorer and select `Aventus : Create...`.
---
+++ template.avt.ts
@@ -0,0 +1,43 @@
+export class Template extends AventusTemplate {
+    protected override meta(): TemplateInfo {
+        return {
+            name: "AventusSharp.Project",
+            installationFolder: "AventusSharp/Project",
+            description: "Create an AventusSharp project",
+            organization: "Cobwebsite",
+            isProject: true,
+            tags: ["AventusSharp", "Project"],
+            version: "0.0.1",
+            allowQuick: false,
+            documentation: "https://aventussharp.com",
+            repository: "https://github.com/Cobwebsite/AventusSharp-Projects",
+        };
+    }
+    protected override async run(destination: string): Promise<void> {
+
+        const name = await this.input({ placeHolder: "Provide a name for your project" });
+        if(!name) return;
+        this.registerVar("projectName", name);
+
+        let prefix = await this.input({
+            placeHolder: "Provide a component prefix : (default is av)",
+            validations: [{
+                message: "Provide a valid prefix",
+                regex: "^(?:[a-z]{2,})?$"
+            }]
+        });
+        if(prefix == null) return;
+        if(!prefix) {
+            prefix = "av";
+        }
+
+        this.registerVar("componentPrefix", prefix);
+        
+        await this.writeFile();
+
+        await this.exec("dotnet add package AventusSharp");
+        await this.exec("dotnet add package Newtonsoft.Json");
+
+    }
+
+}
